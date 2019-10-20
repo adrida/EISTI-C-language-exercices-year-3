d1b498cafbdb5e50654b82e41a4b880f4e4dc45c
Adam Rida <adam.rida@eisti.fr>
2019-10-20 20:09:08 +0200

    rendu fahad

---
+++ tp2/rendufahad.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int	saisienb()
+{
+	int nb = 0;
+	printf("Saisir nb\n");
+	scanf("%d", &nb);
+	return nb;
+}
+
+int main()
+{
+	printf("Votre nombre est %d\n", saisienb());
+	return 0;
+}
