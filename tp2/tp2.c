c4a2b2384c76459a9538ae5c587b9abac586b762
Adam Rida <adam.rida@eisti.fr>
2019-10-29 17:42:05 +0100

    update

--- tp2/tp2.c
+++ tp2/tp2.c
@@ -7,7 +7,7 @@
 
 /* Inclusion des entetes de libraire */
 #include <stdio.h>
-
+#include <math.h>
 /*! \fn void	saisienb()
  * \author Adam Rida <ridaadam@eisti.eu>
  * \version 0.1
@@ -249,6 +249,122 @@ void	pred_date()
 }
 
 
+/*! \fn void	fact(void)
+* \author Adam Rida <ridaadam@eisti.eu>
+* \version 0.1
+* \date 2019-10-29
+* \brief Fonction qui calcule puis affiche la factorielle d'un entier saisi 
+*/
+void	fact(void)
+{
+	int n = 0;
+	int res = 1;
+	int i;
+	printf("Entrez votre nombre :\n");
+	scanf("%d", &n);
+	i = n;
+	if (n <= 0)
+		return;
+	if (n == 0)
+		printf("%d! = %d\n", n, res); 
+	else
+	{
+		while (n != 0)
+		{
+			res = n * res;
+			n--;
+		}
+		printf("%d! = %d\n", i, res); 
+	}
+	return;
+}
+
+/*! \fn void	pgcd(void)
+* \author Adam Rida <ridaadam@eisti.eu>
+* \version 0.1
+* \date 2019-10-29
+* \brief Fonction qui calcule et affiche le pgcd de deux entiers saisis
+*/
+void	pgcd(void)
+{
+
+	int a = 1;
+	int b = 1;
+	int r = 0;
+	printf("Entrez votre nombre a :\n");
+	scanf("%d", &a);
+	printf("Entrez votre nombre b :\n");
+	scanf("%d", &b);
+	while (b != 0)
+	{
+		r = a % b;
+		a = b;
+		b = r;
+	}
+	printf("Le PGCD est %d\n", a); 
+	return;
+}
+
+/*! \fn void	eq2nd(void)
+* \author Adam Rida <ridaadam@eisti.eu>
+* \version 0.1
+* \date 2019-10-29
+* \brief Fonction qui trouve et affiche les racines d'une equation du second degre 
+*/
+void	eq2nd(void)
+{
+	
+	float a = 0;
+	float b = 0;
+	float c = 0;
+	float d = 0;
+	printf("Entrez le coeff a :\n");
+	scanf("%e", &a);
+	printf("Entrez le coeff b :\n");
+	scanf("%e", &b);
+	printf("Entrez le coeff c :\n");
+	scanf("%e", &c);
+	d = (b * b) - (4 * a * c);
+	if (d > 0)
+		printf("La racine 1 est : %f\nLa racine 2 est : %f", ((-b - sqrt(d)) / (2 * a)) , ((-b + sqrt(d)) / (2 * a)));
+	if (d == 0)
+		printf("La racine est : %f\n",((-b) / (2 * a)));
+	if (d < 0)
+		printf ("Pas de racines reelles.\n");
+	return;
+
+}
+
+
+/*! \fn void	menu(void)
+* \author Adam Rida <ridaadam@eisti.eu>
+* \version 0.1
+* \date 2019-10-29
+* \brief Question 9. Menu qui permet de realiser les differentes operations mathematiques proposees
+* \remark Il s'agit d'un menu recursif, il aurait aussi ete possible de le faire grace a une boucle while. ATTENTION, ne pas oublier -lm lors de la compilation pour la fonction sqrt 
+*/
+void	menu(void)
+{
+	int input = 1;
+
+	printf("\n\n                         ========== MENU ===========\n"); 
+	printf("\n                        1 --- Calcul de factorielle \n");
+	printf("                        2 --- PGCD \n");
+	printf("                        3 --- Equation second degre \n");
+	printf("                        0 ---> Quitter \n\n");
+	printf("Faites votre choix : \n");
+	scanf("%d", &input);
+	if (input == 0)
+		return;
+	if (input == 1)
+		fact();
+		if (input == 2)
+			pgcd();
+		if (input == 3)
+			eq2nd();
+	menu();
+}
+
 int main()
 {
 
@@ -267,10 +383,9 @@ int main()
 	printf("\n------ QUESTION 7 : Predire l'heure -------\n");
 //	pred_heure();
 	printf("\n------ QUESTION 8 : Predire la date -------\n");
-	pred_date();
-
-
-
+//	pred_date();
+	printf("\n------ QUESTION 9 : Operations mathematiques -------\n");
+	menu();
 
 	return 0;
 }
