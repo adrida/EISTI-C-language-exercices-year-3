41f2916e7d947f665dfcfa6711d80f120675e84c
Adam Rida <adam.rida@eisti.fr>
2019-11-04 17:24:12 +0100

    ex01 doc doxyfile

--- tp3/ex01.c
+++ tp3/ex01.c
@@ -1,6 +1,22 @@
+/*!
+* \file ex01.c
+* \brief 
+* \author Adam Rida <ridaadam@eisti.eu>
+* \version 0.1
+* \date 2019-11-04
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
+/*!
+* \fn	int saisieEntier(void)
+* \author Adam Rida <ridaadam@eisti.eu>
+* \version 0.1
+* \date 2019-11-04
+* \brief : Saisie et retour d'un entier 
+*
+* \return : Retourne l'entier saisi 
+*/
 int saisieEntier(void)
 {
 	int n;
@@ -12,6 +28,13 @@ int saisieEntier(void)
 	return (n);	
 }
 
+/*!
+* \fn  void	Saisie(void)
+* \author Adam Rida <ridaadam@eisti.eu>
+* \version 0.1
+* \date 2019-11-04
+* \brief : Saisie d'un entier et l'affiche 
+*/
 void	Saisie(void)
 {
 	int n;
@@ -19,6 +42,17 @@ void	Saisie(void)
 	printf("%d\n",saisieEntier());
 }
 
+/*!
+* \author Adam Rida <ridaadam@eisti.eu>
+* \version 0.1
+* \date 2019-11-04
+*
+* \brief : Fonction principale du fichier, lancement de la fonction Saisie(); 
+* \param ac : nombre d'arguments 
+* \param av : arguments
+*
+* \return : retourne 0 si tout se passe bien
+*/
 int	main(int ac, char **av)
 {
 	Saisie();	
