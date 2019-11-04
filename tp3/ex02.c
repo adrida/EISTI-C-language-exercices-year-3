6fa9732da5ab57a25b39d5a14afa288b62bf7d0e
Adam Rida <adam.rida@eisti.fr>
2019-11-04 17:31:35 +0100

    doc doxygen

--- tp3/ex02.c
+++ tp3/ex02.c
@@ -1,5 +1,19 @@
+/*!
+* \file ex02.c
+* \brief : exercice 2 tp3, portee d'une variable
+* \author Adam Rida <ridaadam@eisti.eu>
+* \version 0.1
+* \date 2019-11-04
+*/
 #include <stdio.h>
 
+/*! 
+* \fn  void	bidon(void)
+* \author Adam Rida <ridaadam@eisti.eu>
+* \version 0.1
+* \date 2019-11-04
+* \brief : Fonction pour experimenter la portee des variables 
+*/
 void	bidon(void)
 {
 	int n;
@@ -8,9 +22,18 @@ void	bidon(void)
 	printf("input2 = %d\n", n);
 }
 
-
-
-
+/*!
+* \fn int	main(int ac, char **av)
+* \author Adam Rida <ridaadam@eisti.eu>
+* \version 0.1
+* \date 2019-11-04
+*
+* \brief : Fonction principale, lance les fonctions de l'ennonce 
+* \param ac : Nb d'arguments
+* \param av : Arguments
+*
+* \return : Retourne 0 si tout se passe bien
+*/
 int	main(int ac, char **av)
 {
 	int n;
