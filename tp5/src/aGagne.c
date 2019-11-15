bdd4242bc114e55e268d25f74e6d187cf6737c83
Adam Rida <adam.rida@eisti.fr>
2019-11-15 10:29:57 +0100

    update

---
+++ tp5/src/aGagne.c
@@ -0,0 +1,49 @@
+/*!
+* \file aGagne.c
+* \brief : Verifie si joueur a gagne dans le plateau de jeu actuel
+* \author Adam Rida <ridaadam@eisti.eu>
+* \version 0.1
+* \date 2019-11-15
+*/
+
+#include "../lib.h"
+
+int check_lines(int ttint_plateau[N][N])
+{
+	int i;
+	int j;
+	int tem;
+	
+	i = 0;
+	j = 0;
+	tem = 1;
+	while (i != N)
+	{
+		j = 0;
+		tem = 1;
+		ck = ttint_plateau[i][0];
+		while (j != N)
+		{
+			if (ttint_plateau[i][j] != ttint_plateau[i][0])
+				tem = 0;
+		}
+		if 
+		
+	}
+}
+
+
+int check_lines(int ttint_plateau[N][N])
+{
+}
+
+
+int check_lines(int ttint_plateau[N][N])
+{
+}
+
+int	aGagne(int ttint_plateau[N][N])
+{
+	
+}
+
